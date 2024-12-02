080d0e48509e368a8ba6c9f39ced42a29458f34f
Ghto007 <144988529+Ghto007@users.noreply.github.com>
2024-12-02 17:02:46 +0200

    Add files via upload

---
+++ terraform/main.tf
@@ -0,0 +1,78 @@
+terraform {
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+backend "s3" {
+    bucket = "lab6-7-my-tf-state"
+    key = "terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "lab6-7-my-tf-lockid" 
+
+  }
+}
+
+# Configure the AWS provider
+provider "aws" {
+  region     = "us-east-1"
+}
+
+resource "aws_security_group" "web_app" {
+  name        = "web_app"
+  description = "security group"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags= {
+    Name = "web_app"
+  }
+}
+
+resource "aws_instance" "web_instance" {
+  ami           = "ami-0866a3c8686eaeeba"
+  instance_type = "t3.micro"
+  security_groups = ["web_app"]
+
+  user_data = <<-EOF
+  #!/bin/bash
+  curl -fsSL https://get.docker.com -o get-docker.sh
+  sudo sh get-docker.sh
+  sudo groupadd docker
+  sudo usermod -aG docker $USER
+  newgrp docker
+  docker pull  ghto007/aws:latest
+  docker run -it ghto007/aws:latest
+
+  EOF
+
+
+  tags = {
+    Name = "webapp_instance"
+  }
+}
+
+
+output "instance_public_ip" {
+  value     = aws_instance.web_instance.public_ip
+  sensitive = true
+}
